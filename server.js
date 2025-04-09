8dbe82de6a1591ac3693d1383c2e6e5b93d9df13
nangirakimberly09 <nangirakimberly09@gmail.com>
2025-04-09 17:11:38 +0300

    FINAL FIX: Use correct mongoURI for database connection in server.js

--- server.js
+++ server.js
@@ -14,14 +14,12 @@ let image = require('./routes/image');
 const app = express();
 
 // connecting the database
-
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env];
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true}, (err)=> {
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true}, (err)=> {
 if (err){
-	console.log(err);
-	} else {
-	console.log('Connected to Database: ${MONGODB_URI}');
-	}
+    console.log(err);
+    } else {
+    console.log(`Connected to Database: ${mongoURI}`);
+    }
 });
 
 // test if the database has connected successfully
@@ -39,13 +37,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 // body parser middleware
 app.use(express.json())
 
-
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
